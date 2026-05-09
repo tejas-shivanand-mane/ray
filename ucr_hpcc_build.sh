9dba99f8937a41439c13683e8fc637d6ab1403b6
tejas <tejas.shivanand.mane@gmail.com>
2026-05-09 14:57:44 -0700

    ic

--- ucr_hpcc_build.sh
+++ ucr_hpcc_build.sh
@@ -1,4 +1,6 @@
-conda init
-conda activate ray-dev
+#!/bin/bash
+source ~/.bashrc
+export PATH=/rhome/tmane002/.conda/envs/ray-dev/bin:$PATH
+
 cd ~/work/ray
-pip install -e python/ 2>&1
+RAY_BAZEL_BUILD_OPTIONS="--jobs=4" pip install -e python/ 2>&1 | tee ~/ray-build.log | grep -E "^\[|ERROR|fatal|Killed"
