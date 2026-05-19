887d4a9fa9fb968fcb803df77f986aa6cea7cfc4
Zack Donder <zackdonder@Mac.lan>
2026-05-18 21:12:05 -0700

    ic

--- gossip_benchmarks/throughput_latency_vs_time.py
+++ gossip_benchmarks/throughput_latency_vs_time.py
@@ -10,7 +10,7 @@ os.environ["RAY_BACKEND_LOG_LEVEL"] = "warning"
 os.environ["RAY_DEDUP_LOGS"] = "0"
 
 SIGNAL_FILE   = "/rhome/tmane002/ready_to_kill.txt"
-RESULT_FILE   = "/rhome/tmane002/results/throughput_latency_vs_time.csv"
+RESULT_FILE   = "/rhome/tmane002/results/thput_lat_with_gossip.csv"
 INTERVAL      = 1.0   # sampling interval in seconds
 TOTAL_TIME    = 120   # total experiment duration
 KILL_AT       = 60    # kill owner at this elapsed time
